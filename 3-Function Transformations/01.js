be3ff6342e36f3d9b0d2f0eefb6df1954c328d8c
Muhammad Rameez <buildwithrameez@gmail.com>
2026-09-18 18:55:03 +0500

    function transformations questions complete

---
+++ 3-Function Transformations/01.js
@@ -0,0 +1,30 @@
+/**
+ * Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.
+
+The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
+
+The function composition of an empty list of functions is the identity function f(x) = x.
+
+You may assume each function in the array accepts one integer as input and returns one integer as output.
+ */
+
+var compose = function(functions) {
+
+    return function(x) {
+        
+        for (let i = functions.length - 1; i >= 0; i--) {
+            x = functions[i](x);
+        }
+
+        return x;
+    };
+};
+
+const functions = [x => x + 1, x => x * x, x => 2 * x];
+const x = 4;
+
+// Pass the array and function into your 'map' function, then log the outcome
+const composedFunction = compose(functions);
+const output = composedFunction(x); 
+
+console.log(output);
---
+++ 3-Function Transformations/03.js
@@ -0,0 +1,30 @@
+/**
+ * Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+The first time the returned function is called, it should return the same result as fn.
+Every subsequent time it is called, it should return undefined.
+ */
+
+var once = function(fn) {
+
+    let called = false;
+    
+    return function(...args){
+
+        if (!called) 
+        { 
+            called = true; return fn(...args);
+        } 
+        
+        return undefined;
+    }
+};
+
+const fn = (a,b,c) => (a + b + c);
+const calls = [[1,2,3],[2,3,6]]
+
+const composedFunction = once(fn); 
+
+const output = calls.map(args => composedFunction(...args)); 
+
+console.log(output); 
